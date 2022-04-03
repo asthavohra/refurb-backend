f4219941b359baa0a2c8e109243f03a9d570be95
astha vohra <vohraastha.2712@gmail.com>
2022-04-03 20:51:12 +0100

    fixed stripe key issue

--- routes/PaymentRoute.js
+++ routes/PaymentRoute.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-
-const stripe = require("stripe")(stripeSecretKey);
 
 router.post("/payment", async (request, response) => {
+  const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
   const total = request.body.total;
   const paymentIntent = await stripe.paymentIntents.create({
     amount: total, // subunits of the currency
